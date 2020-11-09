965d331d3031d3170935185c25f50b0cb2b00565
Shay P.C <3180102686@zju.edu.cn>
2020-11-09 17:19:30 +0800

    texture binding enabled

--- shaders/1.color.fs
+++ shaders/1.color.fs
@@ -1,14 +1,19 @@
 #version 330 core
 out vec4 FragColor;
-  
+struct Material{
+    sampler2D diffuse;
+    sampler2D specular;
+    float shininess;
+};
 in vec3 Normal;
 in vec3 FragPos;
+in vec2 TexCoords;
 
 uniform vec3 lightPos;
 uniform vec3 objectColor;
 uniform vec3 lightColor;
 uniform vec3 viewPos;
-
+uniform Material material;
 void main()
 {
     float ambientStrength =0.1;
@@ -23,8 +28,8 @@ void main()
     float diff = max(dot(norm,lightDir),0.0);
     vec3 diffuse = diff * lightColor;
 
-    float spec = pow(max(dot(viewDir,reflectDir),0.0),16);
+    float spec = pow(max(dot(viewDir,reflectDir),0.0),material.shininess);
     vec3 specular = specularStrength * spec * lightColor;
-    vec3 result = (ambient + diffuse+specular) * objectColor;
+    vec3 result = (ambient + diffuse) * texture(material.diffuse,TexCoords).rgb+specular*texture(material.specular,TexCoords).rgb;
     FragColor = vec4(result, 1.0);
 }
