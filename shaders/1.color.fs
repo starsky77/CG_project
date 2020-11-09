beef7590445658fbeaf001c04098b645e106c3a7
Shay P.C <3180102686@zju.edu.cn>
2020-11-09 16:38:59 +0800

    ambient and specular lighting

---
+++ shaders/1.color.fs
@@ -0,0 +1,30 @@
+#version 330 core
+out vec4 FragColor;
+  
+in vec3 Normal;
+in vec3 FragPos;
+
+uniform vec3 lightPos;
+uniform vec3 objectColor;
+uniform vec3 lightColor;
+uniform vec3 viewPos;
+
+void main()
+{
+    float ambientStrength =0.1;
+    float specularStrength = 0.5;
+
+    vec3 ambient= ambientStrength * lightColor;
+    
+    vec3 norm = normalize(Normal);
+    vec3 viewDir= normalize(viewPos-FragPos);
+    vec3 lightDir = normalize(lightPos-FragPos);
+    vec3 reflectDir = reflect(-lightDir,norm);
+    float diff = max(dot(norm,lightDir),0.0);
+    vec3 diffuse = diff * lightColor;
+
+    float spec = pow(max(dot(viewDir,reflectDir),0.0),16);
+    vec3 specular = specularStrength * spec * lightColor;
+    vec3 result = (ambient + diffuse+specular) * objectColor;
+    FragColor = vec4(result, 1.0);
+}
